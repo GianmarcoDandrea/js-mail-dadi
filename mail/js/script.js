c794593374dce3174172f1867ea7253649866490
GianmarcoDandrea <gianm96@hotmail.it>
2023-10-23 17:20:13 +0200

    add: email exercise js file

---
+++ mail/js/script.js
@@ -0,0 +1,24 @@
+const emailAuthorized = ["vakevi6421@gmail.com" , "gianmarco@gmail.com" , "songoku@gmail.com" , "sayanprince@gmail.com" , "iloveramen@gmail.com" , "doitboy@gmail.com" , "gianluca@gmail.com" , "matteo@gmail.com" , "loris@gmail.com" , "luca@gmail.com"];
+
+const userEmail = prompt ("Inserisci la tua email per l'accesso");
+
+let result = false;
+
+for (i=0 ; i < emailAuthorized.length ; i++) {
+    const curEmail = emailAuthorized[i];
+    
+    if (curEmail === userEmail) {
+        result = true;
+    }
+}
+
+console.log(result);
+
+let message ="";
+if (result === true) {
+    message = "Puoi accedere al servizio";
+} else {
+    message = "Non puoi accedere al serizio"
+}
+
+console.log(message);
